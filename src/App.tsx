ffa28a8429b0f608abe5f9b48c495d9eff57e4f4
François Vandal <aVandalInShorts@users.noreply.github.com>
2026-01-13 19:29:15 -0500

    About

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Footer } from "./modules/Footer/Footer";
 import { Header } from "./modules/Header/Header";
+import { About } from "./sections/About/About";
 import { Contact } from "./sections/Contact/Contact";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<About />
 			<Contact />
 			<Footer />
 		</>
---
+++ src/sections/About/About.tsx
@@ -0,0 +1,36 @@
+import styles from "./About.module.css";
+
+export const About = () => {
+	return (
+		<section className={styles.about + " section section-alt-bg"}>
+			<div className={styles["about-inner"] + " section-inner"}>
+				<h2 className={styles.title}>About</h2>
+				<div className={styles.description}>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						Sed do eiusmod tempor incididunt ut labore et dolore
+						magna aliqua.
+					</p>
+					<p>
+						Ut enim ad minim veniam, quis nostrud exercitation
+						ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+				</div>
+				<ul className={styles.list}>
+					<li>
+						<div className="h3">1000 d'expérience!</div>
+						<p>Dans le domaine du développement web.</p>
+					</li>
+					<li>
+						<div className="h3">Plein de projets!</div>
+						<p>Dans le domaine du développement web.</p>
+					</li>
+					<li>
+						<div className="h3">1000 d'expérience!</div>
+						<p>Dans le domaine du développement web.</p>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
+};
