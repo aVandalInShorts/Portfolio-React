9c20478a2d5d5b006c0ec1a4ba533073b04f39d6
François Vandal <aVandalInShorts@users.noreply.github.com>
2026-01-20 19:26:29 -0500

    Versioning untracked files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Footer } from "./modules/Footer/Footer";
 import { Header } from "./modules/Header/Header";
 import { About } from "./sections/About/About";
@@ -5,8 +6,34 @@ import { Contact } from "./sections/Contact/Contact";
 import { Hero } from "./sections/Hero/Hero";
 import { Projects } from "./sections/Projects/Projects";
 import { Skills } from "./sections/Skills/Skills";
+import { getPage } from "./api/strapi";
 
 function App() {
+	const [homePageData, setHomePageData] = useState(null);
+
+	useEffect(() => {
+		const params = new URLSearchParams();
+		params.append("populate[Content][on][blocks.about][populate]", "*");
+		params.append("populate[Content][on][blocks.contact][populate]", "*");
+		params.append(
+			"populate[Content][on][blocks.features-projects][populate]",
+			"*",
+		);
+		params.append("populate[Content][on][blocks.hero][populate]", "*");
+		params.append(
+			"populate[Content][on][blocks.technical-skills][populate]",
+			"*",
+		);
+
+		getPage("homepage", Object.fromEntries(params))
+			.then(setHomePageData)
+			.catch(console.error);
+	}, []);
+
+	useEffect(() => {
+		console.log("homePageData updated:", homePageData);
+	}, [homePageData]);
+
 	return (
 		<>
 			<Header />
